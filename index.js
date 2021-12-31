b507e28b48c1dd36e96295a46061d469479f7aa4
tebellonamo@gmail.com <tebellonamo@gmail.com>
2021-12-31 13:06:01 +0200

    modified the script to add interaction with the dpsa website

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const puppeteer = require('puppeteer');
     async function main() {
+        const idNumber = '9005085373082';
         const browser = await puppeteer.launch({
             headless: false
         });
         const page = await browser.newPage();
         await page.goto('https://www.dpsa.gov.za/psearch/');
+        await page.type('#searchstatus', '9005085373082');
+        await page.click('#header-search-submit');
         await page.waitForTimeout(5000); // wait for 5 seconds
         await browser.close();
     }
