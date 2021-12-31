e846f2ccd8e9636b5f0eafed21b72d7cad537aaa
tebellonamo@gmail.com <tebellonamo@gmail.com>
2021-12-31 16:05:25 +0200

    fixed the ID Number request and feedback response

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const puppeteer = require('puppeteer');
     async function main() {
-        const idNumber = '9005085373082';
+        // const idNumber = '9005085373082';
         const browser = await puppeteer.launch({
             headless: false
         });
         const page = await browser.newPage();
         await page.goto('https://www.dpsa.gov.za/psearch/');
-        await page.type('#searchstatus', '9005085373082');
-        await page.click('#header-search-submit');
+        await page.type('input[type="text"]', '9005085373082');
+        await page.click('input[type="submit"]');
+
+        const spanCount = await page.$$eval('span', (span) => {
+            return span[0].textContent});
         await page.waitForTimeout(5000); // wait for 5 seconds
         await browser.close();
     }
+    console.log(spanCount);
     main();
